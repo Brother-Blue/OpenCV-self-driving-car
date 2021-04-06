858ca344ab545a06d85bab3aacd3f1378e020970
gusmuaha <gusmuaha@student.gu.se>
2021-04-06 12:18:25 +0200

    changed name in cpp to hassan for the sake of assignment 6

--- primeChecker/helloworld.cpp
+++ primeChecker/helloworld.cpp
@@ -5,7 +5,7 @@ int main(int argc, char** argv) {
     if (argc == 2)  {
         int number = std::stoi(argv[1]);
         PrimeChecker pc;
-        std::cout << "Thunberg, Hjalmar; " << number << " is a prime number? " << pc.isPrime(number) << std::endl;
+        std::cout << "Mualla, Hassan; " << number << " is a prime number? " << pc.isPrime(number) << std::endl;
     }
     return 0;
 }
