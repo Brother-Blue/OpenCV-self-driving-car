1f9346055939155c830c513473098e016193f573
Hugo Hempel <hugohhempel@gmail.com>
2021-05-03 18:04:19 +0200

    add debug features

--- src/steering.cpp
+++ src/steering.cpp
@@ -89,6 +89,9 @@ int32_t main(int32_t argc, char **argv) {
                 // Wait for a notification of a new frame.
                 sharedMemory->wait();
 
+                // Performance reading start
+                uint64_t startFrame = cv::getTickCount();
+
                 // Lock the shared memory.
                 sharedMemory->lock();
                 {
@@ -97,24 +100,70 @@ int32_t main(int32_t argc, char **argv) {
                     img = wrapped.clone();
                 }
                 // TODO: Here, you can add some code to check the sampleTimePoint when the current frame was captured.
+                std::pair<bool, cluon::data::TimeStamp> timestampFromImage = sharedMemory->getTimeStamp();
+                std::string timestamp = std::to_string(cluon::time::toMicroseconds(timestampFromImage.second));
                 sharedMemory->unlock();
 
-                // Blur the input stream
-                cv::blur(img, imgBlur, cv::Size(3, 3));
-
-                // Apply HSV filter
-                cv::inRange(imgHSV, yellowLow, yellowHigh, imgDebugHSV);
-                cv::inRange(imgHSV, blueLow, blueHigh, imgDebugHSV);
-
-                // Convert BGR -> HSV
-                cv::cvtColor(imgBlur, frameHSV, cv::COLOR_BGR2HSV);
+                // Working out UTC timestamp
+                cluon::data::TimeStamp ts = cluon::time::now();
+                uint32_t seconds = ts.seconds();
+                std::stringstream stream;
+                std::time_t time = static_cast<time_t>(seconds);
+                tm *p_time = gmtime(&time);
+                stream << "Now: "
+                    << p_time->tm_year+1900 // needed to add 1900 because ctime tm_year is current year - 1900
+                    << "-";
+                if (p_time->tm_mon < 10) {
+                    stream << "0";
+                }
+                stream << p_time->tm_mon+1 // based on 0-11 range, +1 to correct
+                    << "-";
+                if (p_time->tm_mday < 10) {
+                    stream << "0";
+                }
+                stream << p_time->tm_mday
+                    << "T";
+                if (p_time->tm_hour < 10) {
+                    stream << "0";
+                }
+                stream << p_time->tm_hour+1 // same as with the month, 0-23 hour range
+                    << ":";
+                if (p_time->tm_min < 10) {
+                    stream << "0";
+                }
+                stream << p_time->tm_min 
+                    << ":";
+                if (p_time->tm_sec < 10) {
+                    stream << "0";
+                }
+                stream << p_time->tm_sec 
+                    << "Z";
+                std::string date = stream.str();
 
                 // Cropped image frame
-                frameCropped = frameHSV(roi);
+                frameCropped = img(roi);
+
+                // Blur the input stream
+                cv::blur(frameCropped, imgBlur, cv::Size(3, 3));
+
+                // Convert BGR -> HSV
+                cv::cvtColor(imgBlur, frameCropped, cv::COLOR_BGR2HSV);
+
+                // Apply HSV filter
+                // TODO: Figure out how to apply both to the same frame
+                // cv::inRange(imgHSV, yellowLow, yellowHigh, imgDebugHSV);
+                cv::inRange(frameHSV, blueLow, blueHigh, frameCropped);
+
+                uint64_t endFrame = cv::getTickCount();
+                // Convert the speed to ms
+                std::string calcSpeed = std::to_string(((endFrame - startFrame) / cv::getTickFrequency()) * 1000);
 
                 // TODO: Do something with the frame.
                 // Example: Draw a red rectangle and display image.
                 cv::rectangle(img, cv::Point(50, 50), cv::Point(100, 100), cv::Scalar(0,0,255));
+                cv::putText(img, date, cv::Point(25, 25), 5, 1, cv::Scalar(255,255,255), 1);
+                cv::putText(img, "TS: " + timestamp, cv::Point(25, 45), 5, 1, cv::Scalar(255,255,255), 1);
+                cv::putText(imgCrop, "Calculation Speed (ms): " + calcSpeed, cv::Point(25, 25), 5, 1, cv::Scalar(255, 255, 255), 1);
 
                 // If you want to access the latest received ground steering, don't forget to lock the mutex:
                 {
